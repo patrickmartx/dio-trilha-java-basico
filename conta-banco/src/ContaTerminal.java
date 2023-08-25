f2ffe0d4a60a1c16138054bf8d7265863eab2de6
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-25 12:56:48 -0300

    Adcionado comentários para auxiliar na documentação do código.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,34 @@
 import java.util.Scanner;
+/**
+ * <h1>ContaTerminal</h1>
+ * A ContaTerminal solicita dados do Usuário para exibir uma mensagem de agradecimento informando dados de sua conta bancária.
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Patrick Martins
+ * @version 1.0
+ * @since 25/08/2023
+ */
 
 public class ContaTerminal {
+    /**
+     * <h1>main</h1>
+     * Método responsável pela execução do programa.
+     * <p>
+     * Sua principal função é exibir em terminal uma mensagem solicitando dados como:
+     * <li>
+     * <ul>Número da conta</ul>
+     * <ul>Número da agência</ul>
+     * <ul>Nome do cliente</ul>
+     * <ul>Saldo na conta do cliente</ul>
+     * </li>
+     * 
+     * @param args;
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        
         Scanner teclado = new Scanner(System.in);
 
         int numeroConta;
@@ -30,6 +57,15 @@ public class ContaTerminal {
 
     }
 
+    /**
+     * <h1>exibeMensagemBoasVindas</h1>
+     * Método responsável por receber as informações solicitadas no método de execução e, com elas, gerar uma mensagem personalizada para o cliente.
+     * 
+     * @param numeroConta = int Número da conta do cliente.
+     * @param numeroAgencia = String Número da agência do cliente
+     * @param nomeCliente = String Nome do cliente
+     * @param saldoNaConta = boolean Saldo na conta do cliente
+     */
     public static void exibeMensagemBoasVindas(int numeroConta, String numeroAgencia, String nomeCliente, double saldoNaConta){
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %2f já está disponível para saque.", 
         nomeCliente, numeroAgencia, numeroConta, saldoNaConta);

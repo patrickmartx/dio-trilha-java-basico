736993098cda8e42333b910923d52526c04e7783
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-25 12:36:07 -0300

    Projeto desafio de código DIO finalizado!

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+
+        int numeroConta;
+        String numeroAgencia;
+        String nomeCliente;
+        double saldoNaConta;
+
+        System.out.print("Por favor, digite o número da sua conta:\n> ");
+        numeroConta = teclado.nextInt();
+
+        teclado.nextLine(); // Descarregando buffer para ler valor String após ler um valor numérico
+
+        System.out.print("Por favor, digite o número da sua agência:\n> ");
+        numeroAgencia = teclado.nextLine();
+
+        System.out.print("Por favor, insira seu nome e sobrenome:\n> ");
+        nomeCliente = teclado.nextLine();
+
+        System.out.print("Por favor, digite o seu saldo:\n> ");
+        saldoNaConta = teclado.nextDouble();
+
+        numeroAgencia = numeroAgencia.replaceAll("\\s+","");
+        nomeCliente = nomeCliente.replaceAll("\\s+","");
+
+        exibeMensagem(numeroConta, numeroAgencia, nomeCliente, saldoNaConta);
+
+    }
+
+    public static void exibeMensagem(int numeroConta, String numeroAgencia, String nomeCliente, double saldoNaConta){
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %2f já está disponível para saque.", 
+        nomeCliente, numeroAgencia, numeroConta, saldoNaConta);
     }
 }
